fb2e20d7674c81366c5c63d8ad94e5fcfaf9d965
dennis-fast <41125032+dennis-fast@users.noreply.github.com>
2019-01-20 15:10:15 +0100

    Update cel2fahr.py
    Add comments and transform to function

--- functions/cel2fahr.py
+++ functions/cel2fahr.py
@@ -1,6 +1,8 @@
-temp_c = input('Temperature in Celsius: ')
-try:
-    temp_f = int(temp_c) * 9 / 5 + 32
-    print("{temp_c}°C = {temp_f}°F".format(temp_c=temp_c, temp_f=temp_f))
-except:
-    print('Enter a valid number: ')
+# This function converts temperature in Celsius (°C) to Fahrenheit (°F)
+def cel2fahr():
+    temp_c = input('Temperature in Celsius: ')
+    try:
+        temp_f = int(temp_c) * 9 / 5 + 32
+        print("{temp_c}°C = {temp_f}°F".format(temp_c=temp_c, temp_f=temp_f))
+    except:
+        print('Enter a valid number: ')
