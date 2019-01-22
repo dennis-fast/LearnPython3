c267faeb8e791140c8ff291aafd3cacf7ee4fd39
Dennis Fast <dennis.fast@hotmail.com>
2019-01-22 10:37:55 +0100

    Added all materials from the course

--- functions/fahr2cel.py
+++
@@ -1,8 +0,0 @@
-# This function converts temperature in Fahrenheit (°F) to Celsius (°C)
-def fahr2cel():
-    temp_f = input('Temperature in Fahrenheit: ')
-    try:
-        temp_c = (int(temp_f) - 32) * 5 / 9
-        print("{temp_f}°F = {temp_c}°C".format(temp_f=temp_f, temp_c=temp_c))
-    except:
-        print('Enter a valid number: ')
