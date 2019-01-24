d13951223226f2738c92d30cd027b9014b72a1b3
Dennis Fast <dennis.fast@hotmail.com>
2019-01-24 11:02:20 +0100

    Changes in .gitignore

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "bla bla bla",
-            "consumer_secret": "bla bla bla",
-            "token_key": "bla bla bla",
-            "token_secret": "bla bla bla"}
+    return {"consumer_key": "jKRbAU6pKLB7SgbvuktlDokFQ",
+            "consumer_secret": "gpPEjf2gI8uLrbnfv7wU9YdqY1qbRaeqUc7jlvLh5qo8v6OZFC",
+            "token_key": "919108078016061440-vJd785pquvsrYRIXLjf4HpCk8nsSlEG",
+            "token_secret": "VHafo5Qso9Ii5zEpOAZVL9gO1CfB9ZC3E1XIWycoMSLJL"}
